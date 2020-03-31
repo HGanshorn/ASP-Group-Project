947ec62ff6deef748f38ced836388e599a649fd1
Hannah Ganshorn <hg662599@southeast.edu>
2020-03-30 20:45:26 -0500

    test product class

---
+++ GroupProject/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Product
+    {
+        //add more content here later
+    }
+}
