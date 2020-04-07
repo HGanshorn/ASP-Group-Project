bca5928ebf8a0f8826268afb08fed172165a03f0
Glenn <gray@southeast.edu>
2020-04-07 13:13:10 -0500

    Adding Glenn Class

---
+++ GroupProject/Models/Glenn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Glenn
+    {
+        // This is a test Class
+
+    }
+}
