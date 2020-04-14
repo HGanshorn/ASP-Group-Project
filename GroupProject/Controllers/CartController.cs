56eb2d12a8a237e9b894a7b5e4b109b8e3dfe740
Tonya Remaklus <tr6683@southeast.edu>
2020-04-14 14:14:54 -0500

    added Cart.cs to models folder (257) and added CartController to controller folder (264-265)

---
+++ GroupProject/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GroupProject.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToActionResult AddToCart(int productID, string returnUrl)
+        {
+            productID product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productID);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productID, string returnUrl)
+        {
+            productID product = repository.Products
+                .FirstOrDefault(product => p.ProductID == productID);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+
+        }
+
+        private CartController GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<cart>("Cart") ?? new Cart();
+            return cart;
+;        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+
+    }
+}
